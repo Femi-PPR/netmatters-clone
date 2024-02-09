43989383de865eaad01438cd2d541d333419f510
Femi Oladapo <femioladapo3@gmail.com>
2024-02-09 20:10:13 +0000

    added functionally to open sidebar when its button is pressed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
+$contentDiv = $(".content");
+
+$(document).ready(() => {
+    if (!$("html").hasClass("no-show")) {
+        $("body").css({ height: "100%", overflow: "hidden" });
+    }
+});
+
 $(".btn-accept").on("click", (event) => {
+    document.cookie = "dontShowPopup=true;max-age=604800;";
     $(".cookie-wrapper").hide();
+    $("body").removeAttr("style");
 });
 
 $(".btn-cookie").on("click", (event) => {
-    console.log("IS this on?");
-    $(".cookie-wrapper").show();
+    $(".cookie-wrapper").css({ display: "flex" });
+    $("body").css({ height: "100%", overflow: "hidden" });
 });
 
 const carouselSlickOption = {
@@ -41,7 +51,7 @@ let scrollStart = NaN;
 function showHideHeader(isSrcollDirectionDown, scrollTop) {
     if (scrollTop > 110) {
         if (isSrcollDirectionDown) {
-            $(".content").prepend($stickyHeader.animate({ top: 0 }, 250));
+            $contentDiv.prepend($stickyHeader.animate({ top: 0 }, 250));
         } else {
             $stickyHeader.animate({ top: "-100%" }, 250, () => {
                 $stickyHeader.remove();
@@ -55,7 +65,6 @@ function showHideHeader(isSrcollDirectionDown, scrollTop) {
 $(document).on("scroll", (event) => {
     clearTimeout(window.scrollEndTimer);
     window.scrollEndTimer = setTimeout(() => {
-        console.log(`end: ${$(this).scrollTop()}`);
         showHideHeader(scrollStart > $(this).scrollTop(), $(this).scrollTop());
         // console.log(
         //     `scrolled ${scrollStart < $(this).scrollTop() ? "up" : "down"}`
@@ -64,9 +73,7 @@ $(document).on("scroll", (event) => {
     }, 100);
     if (Number.isNaN(scrollStart)) {
         scrollStart = $(this).scrollTop();
-        console.log($(this).scrollTop());
     }
-    console.log("tehe");
 });
 
 // document.addEventListener("scrollend", () => {
@@ -77,3 +84,29 @@ $(document).on("scroll", (event) => {
 //     scrollStart = NaN;
 //     window.scrollEndTimer =
 // });
+
+$contentDiv.on("click", (event) => {
+    const $target = $(event.target);
+    const isSidebarActive = $contentDiv.hasClass("sidebar-active");
+    const burgerBtnClicked =
+        $target.hasClass("btn-hamburger") ||
+        $target.parents(".btn-hamburger").length === 1;
+
+    if (!isSidebarActive && burgerBtnClicked) {
+        $contentDiv.addClass("sidebar-active");
+        $stickyHeader.addClass("sidebar-active");
+        $(".hamburger-inner").addClass("sidebar-active");
+    } else if (isSidebarActive) {
+        $contentDiv.removeClass("sidebar-active");
+        $stickyHeader.removeClass("sidebar-active");
+        $(".hamburger-inner").removeClass("sidebar-active");
+    }
+});
+
+// $(".btn-hamburger").on("click", () => {
+//     showSidebar();
+// });
+
+function showSidebar() {
+    $contentDiv.addClass("sidebar-active");
+}

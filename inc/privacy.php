2e33cd6e744b1b24173528e7c4a63a463ed74848
Femi Oladapo <femioladapo3@gmail.com>
2024-03-02 21:03:36 +0000

    added name attribute to checkbox

--- inc/privacy.php
+++ inc/privacy.php
@@ -7,7 +7,7 @@ if (!isset($checkboxID)) {
 
 <label class="checkbox-label" for="<?= $checkboxID ?>">
     <span class="checkbox-border">
-        <input type="checkbox" class="checkbox-privacy" id="<?= $checkboxID ?>">
+        <input type="checkbox" name="<?= $checkboxID ?>" class="checkbox-privacy" id="<?= $checkboxID ?>">
         <i class="mdi-checked"></i>
     </span>
     <strong class="checkbox-text">

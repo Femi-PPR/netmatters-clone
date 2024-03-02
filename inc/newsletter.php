0a49ad2fadbf988f524ac404a275c6ee71dc19cf
Femi Oladapo <femioladapo3@gmail.com>
2024-03-02 21:02:56 +0000

    added input validation newsletter form

--- inc/newsletter.php
+++ inc/newsletter.php
@@ -1,20 +1,55 @@
 <section class="newsletter">
     <div class="container">
-        <form class="form-newsletter" action="index.html">
+        <form class="form-newsletter" id="form-newsletter" method="post" action="#form-newsletter">
+            <?php
+            $checkboxID = "privacy-newsletter";
+            var_dump(function_exists("validFormat"));
+            require_once "inc/validate.php";
+            $errMsgs = [
+                "newletter-name" => [
+                    "required" => "The name field is required."
+                ],
+                "newletter-email" => [
+                    "required" => "The email field is required.",
+                    "format" => "The email format is invalid."
+                ],
+            ];
+
+
+            var_dump($_POST);
+            $successMsg = 'Your message has been sent!';
+            $alertMsgs = getAlertMsgs($errMsgs, $successMsg);
+
+            if (!isset($_POST[$checkboxID])) {
+                $alertMsg = [
+                    'type' => 'error',
+                    'msg' => 'The marketing preference field is required.'
+                ];
+
+                if (count($alertMsgs) > 0 && $alertMsgs[0]['type'] === 'error') {
+                    $alertMsgs[] = $alertMsg;
+                } else {
+                    $alertMsgs = [$alertMsg];
+                }
+            }
+
+            require "inc/msg-area.php";
+            ?>
             <h2>Email Newsletter Sign-Up</h2>
             <div class="inputs-newsletter">
                 <div>
-                    <label class="required" for="name">Your Name</label><input id="name" type="text">
+                    <label class="required" for="name">Your Name</label><input id="name" name="newletter-name"
+                        type="text">
                 </div>
                 <div>
-                    <label class="required" for="email">Your Email</label><input id="email" type="text">
+                    <label class="required" for="email">Your Email</label><input id="email" name="newletter-email"
+                        type="text">
                 </div>
             </div>
             <?php
-            $checkboxID = "pretty-checkbox-newsletter";
-            include 'privacy.php';
-            ?>
 
+            require 'privacy.php';
+            ?>
             <button class="btn">Subscribe</button>
         </form>
     </div>

c27ed90becc50b2b0ab921bbae223f0a77f50737
Femi Oladapo <femioladapo3@gmail.com>
2024-03-03 17:27:11 +0000

    added line so that popup doesnt show when js is disabled

--- inc/head.php
+++ inc/head.php
@@ -8,8 +8,9 @@ if (!isset($title)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>
+        document.querySelector("html").className = "js";
         if (document.cookie.split("; ").find((row) => row.startsWith("dontShowPopup"))) {
-            document.querySelector("html").className = "no-show";
+            document.querySelector("html").className += " no-show";
         }
     </script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
